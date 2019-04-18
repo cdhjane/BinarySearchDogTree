fc0a67b851a90125cbae73b55318964574b28c26
cdhjane <dongbingnan233@126.com>
2019-04-18 12:58:24 +0800

    this ia a test file

---
+++ src/BSTDogProject/Test.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BSTDogProject;
+
+
+/**
+ *
+ * @author Dell
+ */
+public class Test {
+    public String speak() {
+    //TODO: Add code to compute result
+    return "Hello";
+   }
+   public static int largest(int[] list) {
+    int largest = list[0];
+    for (int i = 1; i < list.length; i++) {
+    if (largest < list[i]) {
+      largest = list[i];
+    }
+  }
+    return largest;
+    }
+}
